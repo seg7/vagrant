146dab988256e6cafac8424da595217d142270cd
seg7 <segseven@gmail.com>
2018-05-08 15:17:08 +0100

    Check for composer.json

--- vagrant/puppet/modules/vagrant/manifests/add_vendor.pp
+++ vagrant/puppet/modules/vagrant/manifests/add_vendor.pp
@@ -7,7 +7,10 @@ class vagrant::add_vendor() {
     cwd         => $basedir,
     path        => '/bin:/usr/bin:/usr/local/bin',
     environment => ['HOME=/root'],
-    onlyif      => "test ! -d $basedir/vendor",
+    onlyif      => [
+      "test ! -d $basedir/vendor",
+      "test -e $basedir/composer.json"
+    ],
     timeout     => 0,
     require => [
       Class['::vagrant::add_php'],
