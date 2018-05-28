5d9cae3a967c428b12d505e3c473293811165211
seg7 <segseven@gmail.com>
2018-05-28 11:08:36 +0100

    Apt update & upgrade

--- vagrant/puppet/modules/vagrant/manifests/init.pp
+++ vagrant/puppet/modules/vagrant/manifests/init.pp
@@ -2,4 +2,15 @@ class vagrant() {
 
   lookup('puppet.includes').each |$value| { include "::vagrant::${value}" }
 
+  exec { 'apt-update':
+    command => "/usr/bin/apt-get update",
+  }
+
+  exec { 'apt-upgrade':
+    command => "/usr/bin/apt-get --quiet --yes --fix-broken upgrade",
+    logoutput => "on_failure",
+    path => "/usr/bin:/usr/sbin:/bin:/usr/local/bin:/usr/local/sbin:/sbin",
+    require => Exec['apt-update'],
+  }
+
 }
