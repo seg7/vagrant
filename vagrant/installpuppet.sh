49d1b92d1e429611f98c0c536c2e289025d711ae
seg7 <segseven@gmail.com>
2019-08-23 17:20:56 +0100

    updated .gitignore

--- vagrant/installpuppet.sh
+++ vagrant/installpuppet.sh
@@ -3,11 +3,6 @@
 PUPPETDIR="/opt/puppetlabs/bin"
 
 export DEBIAN_FRONTEND=noninteractive
-#export LANGUAGE=en_US.UTF-8
-#export LANG=en_US.UTF-8
-#export LC_ALL=en_US.UTF-8
-#locale-gen en_US.UTF-8
-#dpkg-reconfigure locales
 
 if [ ! -e "$PUPPETDIR/puppet" ]; then
     curl -O http://apt.puppetlabs.com/puppet5-release-trusty.deb
@@ -22,25 +17,3 @@ PATH="$PATH:$PUPPETDIR"
 
 cd /var/www/vagrant/puppet
 librarian-puppet config tmp /tmp/ --local && librarian-puppet install
-
-#if [[ ! -d "$BASEDIR/vagrant/puppet/modules/php" ]]; then
-#    puppet module install puppet-php --modulepath "$BASEDIR/vagrant/puppet/modules"        # --version 5.3.0
-#fi
-#if [[ ! -d "$BASEDIR/vagrant/puppet/modules/apache" ]]; then
-#    puppet module install puppetlabs-apache --modulepath "$BASEDIR/vagrant/puppet/modules" # --version 3.1.0
-#fi
-#if [[ ! -d "$BASEDIR/vagrant/puppet/modules/mysql" ]]; then
-#    puppet module install puppetlabs-mysql --modulepath "$BASEDIR/vagrant/puppet/modules"  # --version 5.3.0
-#fi
-#if [[ ! -d "$BASEDIR/vagrant/puppet/modules/nodejs" ]]; then
-#    puppet module install puppet-nodejs --modulepath "$BASEDIR/vagrant/puppet/modules"     # --version 5.0.0
-#fi
-#if [[ ! -d "$BASEDIR/vagrant/puppet/modules/ohmyzsh" ]]; then
-#      puppet module install zanloy-ohmyzsh --modulepath "$BASEDIR/vagrant/puppet/modules"    # --version 1.0.3
-#fi
-#if [[ ! -d "$BASEDIR/vagrant/puppet/modules/mailhog" ]]; then
-#    puppet module install ftaeger-mailhog --ignore-dependencies --modulepath "$BASEDIR/vagrant/puppet/modules"   # --version 1.1.0
-#fi
-#if [[ ! -d "$BASEDIR/vagrant/puppet/modules/motd" ]]; then
-#    puppet module install puppetlabs-motd --modulepath "$BASEDIR/vagrant/puppet/modules"    # --version 1.0.3
-#fi
